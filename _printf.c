e0a0641ad92a4ed714eabac16996ff07e330abf3
nouaffo <snouaffo@gmail.com>
2023-04-23 20:22:41 +0000

    Definition of _printf function

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * _printf - print format string
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int len_str;
+	char current_character;
+	va_list ap;
+
+	len_str = 0;
+	va_start(ap, format);
+	if (format == 0)
+		return (0);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			current_character = *format;
+			switch (current_character)
+			{
+				case 'c':
+					len_str += print_chr(va_arg(ap, int));
+					break;
+				case 's':
+					len_str += print_str(va_arg(ap, char *));
+					break;
+				case '%':
+					len_str += write(1, format, 1);
+					break;
+				default:
+					format--;
+					len_str += write(1, format, 1);
+					format++;
+					len_str += write(1, format, 1);
+			}
+		}
+		else
+		{
+			len_str += write(1, format, 1);
+		}
+		format++;
+	}
+	va_end(ap);
+	return (len_str);
+}
