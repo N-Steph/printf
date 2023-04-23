fe8ab3444e8ee18ce01029a8f1ff6d51f83be615
nouaffo <snouaffo@gmail.com>
2023-04-23 20:23:09 +0000

    Some functions used by _printf function

---
+++ sub_routines.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_str - print string to stdout
+ * Return: number of characters of the string
+ */
+int print_str(char *str)
+{
+	char *ptr_temp;
+	int len_str;
+
+	len_str = 0;
+	ptr_temp = str;
+	while (*ptr_temp != '\0')
+	{
+		len_str++;
+		ptr_temp++;
+	}
+	ptr_temp -= len_str;
+	write(1, ptr_temp, len_str);
+	return (len_str);
+
+}
+
+/**
+ * print_str - print a character to stdout
+ * @chr: pointer to character
+ * Return: 1
+ */
+int print_chr(int chr)
+{
+	char temp_chr;
+
+	temp_chr = chr;
+	write(1, &temp_chr, 1);
+	return (1);
+}
