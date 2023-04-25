f81d4f124e1d708d43ee677ea86ae5569daa87c9
nouaffo <snouaffo@gmail.com>
2023-04-25 11:31:20 +0000

    Add function prototypes and structure

--- main.h
+++ main.h
@@ -1,6 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
-int print_str(char *);
-int print_chr(int);
 int _printf(const char *format, ...);
-#endif /* MAIN_h */
+int print_b(unsigned int);
+int print_percent(unsigned int);
+int print_chr(unsigned int);
+int print_str(char *);
+int print_ordinary_char(const char *);
+/**
+ * struct format_spec1 - new datatype
+ * @c: character
+ * @func: function pointer
+ */
+struct format_spec1
+{
+	char c;
+	int (*func)(unsigned int);
+};
+typedef int (*func_ptr)(unsigned int);
+func_ptr prototype02(char);
+#endif /* MAIN_H */
