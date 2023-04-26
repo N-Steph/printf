8c933b33c4d5b04269ff3deffd1f7a95ceba95e2
Rophpad <rophenp@gmail.com>
2023-04-26 05:17:33 -0700

    Change convert prototype

--- convert.c
+++ convert.c
@@ -7,7 +7,7 @@
  *
  * Return: a pointer to the number in desired base
  */
-char *convert(int num, int base)
+char *convert(unsigned int num, int base)
 {
 	char *digits = "0123456789ABCDEF";
 	char *result = (char *)malloc(sizeof(char) * 64);
@@ -16,8 +16,6 @@ char *convert(int num, int base)
 	*ptr = '\0';
 	if (result == NULL)
 		return (NULL);
-	if (num < 0)
-		num = -num;
 	do {
 		*--ptr = digits[num % base];
 		num /= base;
